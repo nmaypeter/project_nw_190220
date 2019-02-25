75283d0b9cb8a9f351ae4b1f7f1b70d9f04f7d49
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-02-25 18:56:19 +0800

    Update getFileResultCForAlgo.py

--- getFileResultCForAlgo.py
+++ getFileResultCForAlgo.py
@@ -1,6 +1,7 @@
-for data_setting in [1]:
-    data_set_name = "email_undirected" * (data_setting == 1) + "WikiVote_directed" * (data_setting == 2) + \
-                    "NetPHY_undirected" * (data_setting == 3)
+for data_setting in [2, 3]:
+    data_set_name = "email_undirected" * (data_setting == 1) + "dnc_email_directed" * (data_setting == 2) + \
+                    "email_Eu_core_directed" * (data_setting == 3) + "WikiVote_directed" * (data_setting == 4) + \
+                    "NetPHY_undirected" * (data_setting == 5)
     for pps in [1, 2, 3]:
         profit = []
         for prod_setting in [1, 2]:
@@ -34,9 +35,10 @@ for data_setting in [1]:
             fw.write(str(line) + "\n")
         fw.close()
 
-for data_setting in [1]:
-    data_set_name = "email_undirected" * (data_setting == 1) + "WikiVote_directed" * (data_setting == 2) + \
-                    "NetPHY_undirected" * (data_setting == 3)
+for data_setting in [2, 3]:
+    data_set_name = "email_undirected" * (data_setting == 1) + "dnc_email_directed" * (data_setting == 2) + \
+                    "email_Eu_core_directed" * (data_setting == 3) + "WikiVote_directed" * (data_setting == 4) + \
+                    "NetPHY_undirected" * (data_setting == 5)
     for pps in [1, 2, 3]:
         profit = []
         for prod_setting in [1, 2]:
